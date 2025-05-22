ea32a0ada9f0e4298d843d174f00c17b943e5f70
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-05-22 20:12:53 +0200

    fix(build): cargo check no longer fails due to build script

--- build.rs
+++ build.rs
@@ -7,5 +7,7 @@ fn main() {
         .args(["build", "-t", "zero2prod:build", "-f", "Dockerfile", "--progress", "plain", "."])
         .output()
         .expect("Failed to build docker image");
-    assert!(output.status.success(), "Failed to build docker image: {}", String::from_utf8_lossy(&output.stderr));
+    if !output.status.success() {
+        println!("cargo::warning=Failed to build docker image!");
+    }
 }
